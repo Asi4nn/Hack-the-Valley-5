612b718eac17f4cc8ba2eba2785b5a91279ce97e
Asi4nn <leo.wang142001@gmail.com>
2021-10-16 19:50:43 -0400

    health bars

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerStats : MonoBehaviour
 {
+    public HealthBar playerHealthBar;
+    public HealthBar enemyHealthBar;
+
+    private int maxHealth = 100;
+    [SerializeField] int playerHealth;
+    [SerializeField] int enemyHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerHealth = maxHealth;
+        playerHealthBar.SetMaxHealth(playerHealth);
+        playerHealthBar.SetHealth(playerHealth);
+
+        enemyHealth = maxHealth;
+        enemyHealthBar.SetMaxHealth(enemyHealth);
+        enemyHealthBar.SetHealth(enemyHealth);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void DamagePlayer(int dmg)
     {
-        
+        playerHealth -= dmg;
+        playerHealthBar.SetHealth(playerHealth);
+    }
+
+    public void DamageEnemy(int dmg)
+    {
+        enemyHealth -= dmg;
+        enemyHealthBar.SetHealth(enemyHealth);
     }
 }
