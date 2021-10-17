b9f27cbbb79ec2cdc5f078ff35ad6f14184263d8
Asi4nn <leo.wang142001@gmail.com>
2021-10-17 11:02:44 -0400

    game over screen

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -2,19 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerStats : MonoBehaviour
 {
     public HealthBar playerHealthBar;
     public HealthBar enemyHealthBar;
 
+    public Text gameOverText;
+    public Button backToMenuButton;
+
+    public GameObject questionUI;
+
     private int maxHealth = 100;
     [SerializeField] int playerHealth;
     [SerializeField] int enemyHealth;
 
+    private void Awake()
+    {
+        gameOverText.gameObject.SetActive(false);
+        backToMenuButton.gameObject.SetActive(false);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        backToMenuButton.onClick.AddListener(GotoMainMenu);
+
         playerHealth = maxHealth;
         playerHealthBar.SetMaxHealth(playerHealth);
         playerHealthBar.SetHealth(playerHealth);
@@ -28,11 +42,32 @@ public class PlayerStats : MonoBehaviour
     {
         playerHealth -= dmg;
         playerHealthBar.SetHealth(playerHealth);
+        if (playerHealth <= 0)
+        {
+            SetGameOverScreenVisible("Defeat...");
+        }
     }
 
     public void DamageEnemy(int dmg)
     {
         enemyHealth -= dmg;
         enemyHealthBar.SetHealth(enemyHealth);
+        if (enemyHealth <= 0)
+        {
+            SetGameOverScreenVisible("Victory!");
+        }
+    }
+
+    void SetGameOverScreenVisible(string text)
+    {
+        gameOverText.text = text;
+        gameOverText.gameObject.SetActive(true);
+        backToMenuButton.gameObject.SetActive(true);
+        questionUI.SetActive(false);
+    }
+
+    void GotoMainMenu()
+    {
+        SceneManager.LoadScene(sceneName: "MainMenu");
     }
 }
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Hack the Valley 5
+# Saturday Evening Studyin'
 
 ## Required Technology
 
@@ -8,3 +8,8 @@ Unity 2020.3.5f1
 
 [Leo Wang](https://github.com/Asi4nn)  
 [Glenn Ye](https://github.com/LegoCityMan7063)
+
+## Design Documents
+
+[Question List](https://docs.google.com/document/d/1zh_fBFyeyxyhIYIlZApoWYhSyUdVnpDqQqATVL1O594/edit?usp=sharing)
+[Concept](https://jamboard.google.com/d/1ZLRj7GfoVPDbZuUtNrSmOGliZp2CXlC1XhL3D7O6D44/edit?usp=sharing)
